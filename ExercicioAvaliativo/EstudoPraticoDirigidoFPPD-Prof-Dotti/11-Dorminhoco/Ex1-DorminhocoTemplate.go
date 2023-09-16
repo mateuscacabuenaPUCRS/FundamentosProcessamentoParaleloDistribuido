e0bde38f5885c55e904ebee3a506810efa0aa94b
Mateus Cacabuena <mateuscacabuena@gmail.com>
2023-09-16 20:29:05 -0300

    fix: dorminhoco

--- ExercicioAvaliativo/EstudoPraticoDirigidoFPPD-Prof-Dotti/11-Dorminhoco/Ex1-DorminhocoTemplate.go
+++ ExercicioAvaliativo/EstudoPraticoDirigidoFPPD-Prof-Dotti/11-Dorminhoco/Ex1-DorminhocoTemplate.go
@@ -8,58 +8,93 @@
 //   O ultimo leva a rolhada ...
 //   ESTE  PROGRAMA NAO FUNCIONA.    É UM RASCUNHO COM DICAS.
 
-
 package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-const NJ = 5           // numero de jogadores
-const M = 4            // numero de cartas
+const NJ = 5 // numero de jogadores
+const M = 4  // numero de cartas
 
-type carta string      // carta é um strirng
+type carta string // carta é um strirng
 
-var ch [NJ]chan carta  // NJ canais de itens tipo carta  
+var ch [NJ]chan carta // NJ canais de itens tipo carta
 
-func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta, ... ) {
-	mao := cartasIniciais    // estado local - as cartas na mao do jogador
-	nroDeCartas := M         // quantas cartas ele tem 
-    cartaRecebida := " "     // carta recebida é vazia
+var bateu [NJ]chan int
+
+func jogador(id int, in chan carta, out chan carta, cartasIniciais []carta) {
+	mao := cartasIniciais // estado local - as cartas na mao do jogador
+	nroDeCartas := M      // quantas cartas ele tem
+	cartaRecebida := carta("")
+	fmt.Printf("Jogador %d cartas na mao: %v\n", id, mao)
 
 	for {
-		if // tenho que jogar
-			{
+		if nroDeCartas == 5 {
 			fmt.Println(id, " joga") // escreve seu identificador
 			// escolhe alguma carta para passar adiante ...
-			// guarda carta que entrou 
+			indiceAleatorio := rand.Intn(nroDeCartas)
+			cartaParaSair := mao[indiceAleatorio]
+
 			// manda carta escolhida o proximo
-			out <- cartaParaSair        
-		} else {  
-			// ...
-			cartaRecebida := <-in   // recebe carta na entrada
-			//  ...
-			// e se alguem bate ?
+			out <- cartaParaSair
+			mao = append(mao[:indiceAleatorio], mao[indiceAleatorio+1:]...)
+			nroDeCartas--
+		} else {
+			cartaRecebida = <-in
+			fmt.Printf("Jogador %d recebeu a carta %s\n", id, cartaRecebida)
+			mao = append(mao, cartaRecebida)
+			nroDeCartas++
+
+			// Verifica se um jogador possui 4 cartas da mesma letra e bate
+			if possuiQuatroLetrasIguais(mao) {
+				fmt.Printf("Jogador %d bate!\n", id)
+				//bateu[id] <- 1
+				return;
+			}
 		}
 	}
 }
 
+func possuiQuatroLetrasIguais(mao []carta) bool {
+	contagem := make(map[carta]int)
+	for _, c := range mao {
+		contagem[c]++
+		if contagem[c] >= 4 {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	rand.Seed(time.Now().UnixNano()) // Inicialize a semente para a geração de números aleatórios
+	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < NJ; i++ {
-		ch[i] = make(chan struct{})
+		ch[i] = make(chan carta)
 	}
 
 	for i := 0; i < NJ; i++ {
-        cartasEscolhidas := make([]carta, 4)
-        for j := 0; j < 4; j++ { // escolhe aleatoriamente (tira) cartas do baralho, passa cartas para jogador
-            indiceAleatorio := rand.Intn(4) // Gera aleatoriamente um índice entre 0 e 3
-            cartasEscolhidas[j] = carta('A' + indiceAleatorio)
-        }
+		cartasEscolhidas := make([]carta, 4)
+		for j := 0; j < 4; j++ { // escolhe aleatoriamente (tira) cartas do baralho, passa cartas para jogador
+			indiceAleatorio := rand.Intn(4) // Gera aleatoriamente um índice entre 0 e 3
+			cartasEscolhidas[j] = carta('A' + indiceAleatorio)
+		}
 
-        go jogador(i, ch[i], ch[(i+1)%NJ], cartasEscolhidas)
-    }
-	
-	<-make(chan struct{}) // bloqueia
+		go jogador(i, ch[i], ch[(i+1)%NJ], cartasEscolhidas)
+	}
+
+	// Comece o jogo passando uma carta inicial
+	cartaInicial := carta("@")
+	ch[0] <- cartaInicial
+
+	// Aguarde todos os jogadores terminarem
+	for i := 0; i < NJ; i++ {
+		<-ch[i]
+	}
+
+	fmt.Println("O jogo terminou!")
+	<-make(chan carta) // bloqueia
 }
